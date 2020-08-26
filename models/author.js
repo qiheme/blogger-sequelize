901692b367b669939ff3ef1846403ab8497948fc
Quincy Iheme <qiheme@gmail.com>
2020-08-25 20:10:57 -0400

    Update author.js

--- models/author.js
+++ models/author.js
@@ -1,4 +1,4 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function(     sequelize, DataTypes) {
   var Author = sequelize.define("Author", {
     // Giving the Author model a name of type STRING
     name: DataTypes.STRING
