07d9a17609d51194c66647e76468d971191ad49d
Quincy Iheme <qiheme@gmail.com>
2020-08-25 20:50:26 -0400

    Fix linting errors

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    name:  DataTypes.STRING
+  const User = sequelize.define("User", {
+    name: DataTypes.STRING
   });
   return User;
 };
--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -6,15 +6,14 @@
 // =============================================================
 
 // Requiring our models
-var db = require("../models");
+const db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-
   // GET route for getting all of the posts
-  app.get("/api/posts", function(req, res) {
-    var query = {};
+  app.get("/api/posts", (req, res) => {
+    const query = {};
     if (req.query.author_id) {
       query.AuthorId = req.query.author_id;
     }
@@ -24,13 +23,13 @@ module.exports = function(app) {
     db.Post.findAll({
       where: query,
       include: [db.Author]
-    }).then(function(dbPost) {
+    }).then(dbPost => {
       res.json(dbPost);
     });
   });
 
   // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
+  app.get("/api/posts/:id", (req, res) => {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Author
@@ -39,38 +38,36 @@ module.exports = function(app) {
         id: req.params.id
       },
       include: [db.Author]
-    }).then(function(dbPost) {
+    }).then(dbPost => {
       res.json(dbPost);
     });
   });
 
   // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    db.Post.create(req.body).then(function(dbPost) {
+  app.post("/api/posts", (req, res) => {
+    db.Post.create(req.body).then(dbPost => {
       res.json(dbPost);
     });
   });
 
   // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
+  app.delete("/api/posts/:id", (req, res) => {
     db.Post.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbPost) {
+    }).then(dbPost => {
       res.json(dbPost);
     });
   });
 
   // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post.update(
-      req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      }).then(function(dbPost) {
+  app.put("/api/posts", (req, res) => {
+    db.Post.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(dbPost => {
       res.json(dbPost);
     });
   });
